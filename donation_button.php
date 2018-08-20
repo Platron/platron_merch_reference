af0ca32670aa7e87a7ca81fdfa03e0c34d4abbb9
v.egorov <v.egorov@platron.ru>
2018-08-20 12:32:21 +0300

    Added html required tags

--- donation_button.php
+++ donation_button.php
@@ -21,19 +21,29 @@ if(!empty($_POST['platron'])){
 }
 
 ?>
-
-<form name="platron_payment" method="POST">
-	<table>
-		<tr>
-			<td>Описание: </td>
-			<td><input type="text" name="description">
-		</tr>
-		<tr>
-			<td>Сумма: </td> 
-			<td><input type="text" name="amount"></td>
-		</tr>
-		<tr>
-			<td colspan="2"><input type="submit" name="platron"><td>
-		</tr>
-	</table>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Donation button example</title>
+</head>
+<body>
+	<form name="platron_payment" method="POST">
+		<table>
+			<tr>
+				<td>Описание: </td>
+				<td><input type="text" name="description">
+			</tr>
+			<tr>
+				<td>Сумма: </td> 
+				<td><input type="text" name="amount"></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" name="platron"><td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
